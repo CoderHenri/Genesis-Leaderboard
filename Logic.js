7ad4c257a46dac4d1e6cf9245a96477ea7004083
Henri0812 <p.metzner@gmx.at>
2020-04-18 14:44:08 +0200

    deleted unnecessary code

--- Logic.js
+++ Logic.js
@@ -96,44 +96,7 @@ function GetLandData() {
     }
     console.log(row, col);
     i++;
-  }
-
-
-
-/*  Important for later
-    var ol = document.getElementById("GenesisLeaderList");
-
-    for( var i = 0; i < data.length; i++ )
-    { 
-       var o = data[i];
-       var li = document.createElement("li");
-       li.appendChild(document.createTextNode(o.title));
-       ol.appendChild(li);    
-    }                   
- */
-/*
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-    },
-    body: JSON.stringify({
-      operationName : "GetLandDetail",
-      variables: { 
-        row, 
-        col,
-      },
-      query: "query GetLandDetail($col: Int!, $row: Int!) {   land(col: $col, row: $row) {     ...LandDetail     __typename   } }  fragment LandDetail on Land {   realTokenId   owner   landType   row   col   bundles   auction {     ...AxieAuction     __typename   }   offers {     ...OfferDetail     __typename   }   ownerProfile {     name     __typename   }   __typename }  fragment AxieAuction on Auction {   startingPrice   endingPrice   startingTimestamp   endingTimestamp   duration   timeLeft   currentPrice   currentPriceUSD   suggestedPrice   seller   listingIndex   auctionType   __typename }  fragment OfferDetail on Offer {   offerer {     ...ProfileBrief     __typename   }   listingIndex   price   createdAt   __typename }  fragment ProfileBrief on AccountProfile {   accountId   addresses {     ...Addresses     __typename   }   email   name   __typename }  fragment Addresses on NetAddresses {   ethereum   tomo   loom   __typename } "
-    })
-  })
-  .then(r => r.json())
-  .then(data => console.log("data returned:", data));
-*/
-
-
-  //https://axieinfinity.com/marketplace-api/query-land?row=-30&col=-22
-
+  }  
 }
 
 //Testbed code:
