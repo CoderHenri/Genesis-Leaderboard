ce8c19a302469203f753fb8fc80da2a01c0d42ea
Henri0812 <p.metzner@gmx.at>
2020-04-18 14:14:57 +0200

    corrected the counting method and invalid plots

--- Logic.js
+++ Logic.js
@@ -33,21 +33,33 @@ function GetLandData() {
     .then(function(data) {
 
       var text = "Invalid";
-      text = data.data.land.ownerProfile.name;
-      if(text == null) {
-        text = data.data.land.owner;
-      } 
+      try {       //catches the error if there is either a street or no land owner account
+        text = data.data.land.ownerProfile.name;
+        if(text == null) {
+          text = data.data.land.owner;
+        } 
 
-      if(text != "Invalid") {
+        if(text != "Invalid") {
+          GenesisPlotOwners.push(text);
+        } 
+      }
+      catch {
+        text = "No Account registered";
         GenesisPlotOwners.push(text);
       }
-
       console.log(GenesisPlotOwners);
       console.log("entire data:", data);
 
-      if(y == 70) {
+      if(y == 81) {
 
         GenesisPlotOwners.sort();
+
+        for(d=0; d < 7; d++){     // Deletes the street plots between the Genesis plots
+          const index = GenesisPlotOwners.indexOf("No Account registered");
+          if (index > -1) {
+            GenesisPlotOwners.splice(index, 1);
+          }
+        }
 
         var current = null;
         var cnt = 0;
@@ -125,3 +137,48 @@ function GetLandData() {
 }
 
 
+function GetLandData2() {
+   
+
+  var GenesisPlotOwners = [];
+  var GenLeaders = [];
+
+  var url = "https://axieinfinity.com/graphql-server/graphql"
+  var col = -35;
+  var row = -8;
+  var i = 0;
+  var y = 0;
+
+  
+    fetch(url, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        operationName : "GetLandDetail",
+        variables: { 
+          row, 
+          col,
+        },
+        query: "query GetLandDetail($col: Int!, $row: Int!) {   land(col: $col, row: $row) {     ...LandDetail     __typename   } }  fragment LandDetail on Land {   realTokenId   owner   landType   row   col   bundles   auction {     ...AxieAuction     __typename   }   offers {     ...OfferDetail     __typename   }   ownerProfile {     name     __typename   }   __typename }  fragment AxieAuction on Auction {   startingPrice   endingPrice   startingTimestamp   endingTimestamp   duration   timeLeft   currentPrice   currentPriceUSD   suggestedPrice   seller   listingIndex   auctionType   __typename }  fragment OfferDetail on Offer {   offerer {     ...ProfileBrief     __typename   }   listingIndex   price   createdAt   __typename }  fragment ProfileBrief on AccountProfile {   accountId   addresses {     ...Addresses     __typename   }   email   name   __typename }  fragment Addresses on NetAddresses {   ethereum   tomo   loom   __typename } "
+      })
+    })
+    .then(function(response) { 
+      return response.json(); 
+    })
+
+    .then(function(data) {
+
+      
+
+      console.log(GenesisPlotOwners);
+      console.log("entire data:", data);
+
+      
+    
+    console.log(row, col);
+    });
+  
+}
